6c2daa833964116c145f355789c47b71fefec419
typovrak <typovrak@gmail.com>
2025-08-05 17:46:22 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,22 @@ in {
 		cp ${./.gitconfig} ${home}/.gitconfig
 		chown ${username}:${group} ${home}/.gitconfig
 		chmod 600 ${home}/.gitconfig
+
+		mkdir ${home}/projects
+		chown ${username}:${group} ${home}/projects
+		chmod 700 ${home}/projects
+
+		mkdir ${home}/projects/freelancing
+		chown ${username}:${group} ${home}/projects/freelancing
+		chmod 700 ${home}/projects/freelancing
+
+		mkdir ${home}/projects/freelancing/comet-data
+		chown ${username}:${group} ${home}/projects/freelancing/comet-data
+		chmod 700 ${home}/projects/freelancing/comet-data
+
+		cp ${./.gitconfig-comet-data} ${home}/projects/freelancing/comet-data/.gitconfig
+		chown ${username}:${group} ${home}/projects/freelancing/comet-data/.gitconfig
+		chmod 600 ${home}/projects/freelancing/comet-data/.gitconfig
 	'';
 
 	environment.systemPackages = with pkgs; [
