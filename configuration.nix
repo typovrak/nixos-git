9b86acc643e1d3425908257cb52381e8c35b77b1
typovrak <typovrak@gmail.com>
2025-08-08 12:02:37 +0200

    split freelancing activities with my nixos-config

--- configuration.nix
+++ configuration.nix
@@ -13,18 +13,6 @@ in {
 		mkdir -p ${home}/projects
 		chown ${username}:${group} ${home}/projects
 		chmod 755 ${home}/projects
-
-		mkdir -p ${home}/projects/freelancing
-		chown ${username}:${group} ${home}/projects/freelancing
-		chmod 755 ${home}/projects/freelancing
-
-		mkdir -p ${home}/projects/freelancing/comet-data
-		chown ${username}:${group} ${home}/projects/freelancing/comet-data
-		chmod 755 ${home}/projects/freelancing/comet-data
-
-		cp ${./.gitconfig-comet-data} ${home}/projects/freelancing/comet-data/.gitconfig
-		chown ${username}:${group} ${home}/projects/freelancing/comet-data/.gitconfig
-		chmod 600 ${home}/projects/freelancing/comet-data/.gitconfig
 	'';
 
 	environment.systemPackages = with pkgs; [
